da920dfdd5d9f275bfa977646d8c6b470ab323ae
supersheeppro <73169248+supersheeppro@users.noreply.github.com>
2025-09-08 13:01:26 +0200

    added requirements.txt

--- src/run.py
+++ src/run.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",      # pad naar je FastAPI app (module:app)
         host="127.0.0.1",    # localhost
-        port=8000,           # poort waarop de site draait
+        port=8090,           # poort waarop de site draait
         reload=True         # auto-reload bij codewijzigingen (debug mode)
     )
